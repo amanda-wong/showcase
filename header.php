e42b9bcaf6e91add4fa78be446fc3bb3e541cab1
Amanda Wong <Amanda Wong>
2016-06-03 15:49:18 -0700

    Add logo to navigation of header.php

--- header.php
+++ header.php
@@ -24,7 +24,6 @@
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 
 						<a id="logo" href="/">Amanda Wong</a>
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
 				</nav><!-- #site-navigation -->
