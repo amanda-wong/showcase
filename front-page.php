78fdeba3d69a728f7891dd9160b66004d796858b
Amanda Wong <Amanda Wong>
2016-06-05 13:48:23 -0700

    Add .page-hero div around front-page.php header

--- front-page.php
+++ front-page.php
@@ -1,11 +1,13 @@
+<div class="page-hero">
 <?php
 
 get_header(); ?>
-
+</div>
 	<div id="primary" class="content-area">
+
 		<main id="main" class="site-main" role="main">
 
-			
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
