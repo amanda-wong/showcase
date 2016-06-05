99c7e3ec5596d312923179c4655ac68e679617c6
Amanda Wong <Amanda Wong>
2016-06-05 16:04:47 -0700

    Add footer content

--- footer.php
+++ footer.php
@@ -11,7 +11,24 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+		            <h3>- Let's get social -</h3>
+		            <div class="social-icon-wrap">
+		                <a href="https://www.linkedin.com/in/missamandawong" target="_blank">
+		                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+		                </a>
+		                <a href="https://www.facebook.com/amanda.wong.184" target="_blank">
+		                    <i class="fa fa-facebook" aria-hidden="true"></i>
+		                </a>
+		                <a href="https://twitter.com/missamandawong" target="_blank">
+		                    <i class="fa fa-twitter" aria-hidden="true"></i>
+		                </a>
+		                <a href="https://www.instagram.com/missamandawong/" target="_blank">
+		                    <i class="fa fa-instagram" aria-hidden="true"></i>
+		                </a>
+		            </div>
+		            <div class="copyright">
+		                Copyright &copy; <span id="year"></span> Amanda Wong
+		            </div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
