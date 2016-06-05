de1a43df6b6351de0b7d2c5fcecda019560160d6
Amanda Wong <Amanda Wong>
2016-06-05 16:18:40 -0700

    Rearrange placement of content and site main on front page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,11 @@
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
 <div class="page-hero">
 <?php
 
 get_header(); ?>
 </div>
-	<div id="primary" class="content-area">
 
-		<main id="main" class="site-main" role="main">
 
 
 
