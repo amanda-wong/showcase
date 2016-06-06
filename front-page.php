577c2a446a9f1afba0f7eacd4e8588f808e9623b
Amanda Wong <Amanda Wong>
2016-06-05 17:26:29 -0700

    Delete spacing on front-page.php

--- front-page.php
+++ front-page.php
@@ -1,15 +1,15 @@
 <div id="primary" class="content-area">
-<main id="main" class="site-main" role="main">
-<div class="page-hero">
-<?php
+	<main id="main" class="site-main" role="main">
+		<div class="page-hero">
+			<?php
 
-get_header(); ?>
-</div>
-
-
+			get_header(); ?>
+		</div>
 
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+
+
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
