d15ef53110223d2ba4c064c915f4326b3b37917a
Amanda Wong <Amanda Wong>
2016-06-07 20:44:04 -0700

    Create about.php template

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: About Page
+ */
+  ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="page-hero">
+			<?php
+
+			get_header(); ?>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
