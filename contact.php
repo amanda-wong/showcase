9b5eca0dbaaf7ff1873eb92b1e14d07511d4cd23
Amanda Wong <Amanda Wong>
2016-06-12 11:50:04 -0700

    Create contact.php page template

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Contact Page
+ */
+  ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="page-hero">
+			<?php get_header(); ?>
+            <div class="hero-content">
+            </div>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
