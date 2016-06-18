91ba139d9661da8c7dae3252c5691becae5616f5
Amanda Wong <Amanda Wong>
2016-06-17 23:14:11 -0700

    Add ease in effect to showMenu/hideMenu function

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ var elMobileMenu = document.getElementById('mobile-menu-container');
 
 function showMenu() {
     elMobileMenu.style.left = '0px';
+    elMobileMenu.style.transition = 'ease 0.3s';
+    // document.getElementsByTagName('')[0] = 'linear 0.3s';
+
 }
 
 function hideMenu() {
     elMobileMenu.style.left = '-250px';
-
-    if (window.screen.width >= 480) {
-        elMobileMenu.style.left = '-250px';
-    }
+    elMobileMenu.style.transition = 'ease 0.3s';
+    // if (screen.width > 480) {
+    //     elMobileMenu.style.display = 'none';
+    // }
 }
