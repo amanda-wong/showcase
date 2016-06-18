5d0e7bc3765626efff43b7847258b9e14c142249
Amanda Wong <Amanda Wong>
2016-06-17 21:58:24 -0700

    Create mobile showMenu and hideMenu functions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function showMenu() {
+    document.getElementById('mobile-menu-container').style.left = '0px';
+}
+
+function hideMenu() {
+    document.getElementById('mobile-menu-container').style.left = '250px';
+}
