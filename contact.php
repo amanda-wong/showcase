bfc9bf7cef8f8bcf5cab5cdd8f5c20f19e896581
Amanda Wong <Amanda Wong>
2016-06-23 22:25:40 -0700

    Add contact content

--- contact.php
+++ contact.php
@@ -12,14 +12,20 @@
 				<h1>Connect with me</h1>
                 <hr/>
 			</div>
+            <section class="contact-container">
+                <div class="contact-details">
+
+                </div>
+                <form name="contact_form" class="contact-form" action="/process_form.php" method="post">
+                    <input type="text" name="name" id="name" placeholder="Name" value="">
+                    <input type="text" name="subject" id="subject" placeholder="Subject" value="">
+                    <input type="email" name="email" id="email" placeholder="Email" value="">
+                    <textarea name="message" rows="8" cols="40" placeholder="Leave a message" value=""></textarea>
+                    <input class="send-message" type="button" name="button" value="Send" action="submit">
+                </form>
+            </section>
 		</div>
-        <!-- <form name="contact_form" class="contact-form" action="/process_form.php" method="post">
-            <input type="text" name="name" id="name" placeholder="Name" value="">
-            <input type="text" name="subject" id="subject" placeholder="Subject" value="">
-            <input type="email" name="email" id="email" placeholder="Email" value="">
-            <textarea name="message" rows="8" cols="40" placeholder="Leave a message" value=""></textarea>
-            <input class="send-message" type="button" name="button" value="Send" action="submit">
-        </form> -->
+
 	</main><!-- #main -->
 </div><!-- #primary -->
 
