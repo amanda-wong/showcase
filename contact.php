69281b624eb3ada48a7352f41bbc4b02d5355948
Amanda Wong <Amanda Wong>
2016-06-26 18:09:42 -0700

    Style contact content and form

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@
                 <hr/>
 			</div>
         </div>
-        <!-- <section class="contact-container">
+        <section class="contact-container">
             <div class="contact-content">
                 <h2>Say hello</h2>
                 <p>
@@ -22,29 +22,35 @@
                     Feel free to reach out, I'd love to hear from you!
                 </p>
             </div>
-                <form name="contact_form" class="contact-form" action="/process_form.php" method="post">
-                    <label>Full Name *</label>
-                    <div class="name-wrap">
-                        <div class="fname-wrap">
-                            <input type="text" name="firstName" id="first-name" value="">
-                            <label for="firstName" required>First Name</label>
-                        </div>
-                        <div class="lname-wrap">
-                            <input type="text" name="lastName" id="last-name" value="">
-                            <label for="lastName" required>Last Name</label>
-                        </div>
+            <form name="contact_form" class="contact-form" action="/process_form.php" method="post">
+                <label>
+                    Full Name <span>*</span>
+                </label>
+                <div class="name-wrap">
+                    <div class="fname-wrap">
+                        <input type="text" name="firstName" id="first-name" value="">
+                        <label for="firstName" required>First Name</label>
                     </div>
-                    <div class="email-wrap">
-                        <label for="email">Email Address *</label>
-                        <input type="email" name="email" id="email" value="">
+                    <div class="lname-wrap">
+                        <input type="text" name="lastName" id="last-name" value="">
+                        <label for="lastName" required>Last Name</label>
                     </div>
-                    <div class="message-wrap">
-                        <label for="email">Your Message *</label>
-                        <textarea name="message" rows="8" cols="40" value=""></textarea>
-                    </div>
-                    <input class="send-message" type="button" name="button" value="Send" action="submit">
-                </form>
-        </section> -->
+                </div>
+                <div class="email-wrap">
+                    <label for="email">
+                        Email Address <span>*</span>
+                    </label>
+                    <input type="email" name="email" id="email" value="">
+                </div>
+                <div class="message-wrap">
+                    <label for="email">
+                        Your Message <span>*</span>
+                    </label>
+                    <textarea name="message" rows="8" cols="40" value=""></textarea>
+                </div>
+                <input class="send-message" type="button" name="button" value="Send" action="submit">
+            </form>
+        </section>
         <?php get_footer(); ?>
 	</main><!-- #main -->
 </div><!-- #primary -->
