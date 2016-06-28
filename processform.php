ad6f4ae09e48068496085adeeccc5b4929c00c9d
Amanda Wong <Amanda Wong>
2016-06-28 00:41:28 -0700

    Edit form handling file

--- processform.php
+++ processform.php
@@ -1,24 +1,26 @@
-<?php
-$name = $_POST['firstName'] . ' ' . $_POST['lastName'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$to = "missamandawong@gmail.com";
-$subject = "New Message from " . $name;
+    <?php
 
-var validation = function() {
-    alert('you forgot to tell me your name!');
+    //Form information
+    $name = $_POST['firstName'] . ' ' . $_POST['lastName'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $to = 'missamandawong@gmail.com';
+    $subject = 'New WordPress Message';
+
+    $body = <<<EOD
+<br><hr><br>
+Email: $email<br>
+Name: $name<br>
+Message: $message<br>
+EOD;
+
+    $headers = "From: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+    $success = mail($to, $subject, $body, $headers);
+    // Results rendered as HTML
+
+    if ($success){
+        // print "<meta http-equiv=\"refresh\" content=\"0;URL=thankyou.php\">";
+        alert('Your message has been sent!');
 }
-
- // send email
- $success = mail($to, $subject, $message, "From: " . $name);
- $unsuccessful =
-
- // redirect to success page
- if (isset($_POST['submit']) AND $_POST['email'] != ''){
-     mail($to, $subject, $message, "From: " . $name);
-     alert("Thank you for your message!");
- }
- else{
-    validation();
- }
  ?>
