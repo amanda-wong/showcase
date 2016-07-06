2af2d88d585778cccf5e15192a6c95e5d2f09464
Amanda Wong <Amanda Wong>
2016-07-05 19:58:14 -0700

    Edit front-page h1 text

--- front-page.php
+++ front-page.php
@@ -5,7 +5,8 @@
 
 			get_header(); ?>
 			<div class="welcome-message">
-				<h1>Hi, I'm Amanda and I'm a front-end developer!<br>
+				<h1>Hi, I'm Amanda Wong and
+					I'm a front-end web developer!<br>
 					Come check out some of my work.
 				</h1>
 				<a href="/portfolio" class="fp-cta">Portfolio</a>
