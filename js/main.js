948a4f7582be2e6a80f6ad7003772495e22b2dc4
Amanda Wong <Amanda Wong>
2016-07-09 22:34:17 -0700

    Comment out script to wrap img with div

--- js/main.js
+++ js/main.js
@@ -17,18 +17,18 @@ function hideMenu() {
     elMobileMenu.style.left = '-250px';
 }
 
-/* Wrap Div around Post Images */
+/* Wrap Div Around Post Images */
 
-var blogHeader = document.querySelectorAll('.entry-header');
-
-for (var i = 0 ; i < blogHeader.length ; i++ ) {
-    var currentHeader = blogHeader[i];
-    var headerImg = currentHeader.querySelector('img');
-    if (!headerImg) {
-        continue;
-    }
-
-    var blogImgWrap = document.createElement('div');
-    blogImgWrap.appendChild(headerImg);
-    currentHeader.insertBefore(blogImgWrap, currentHeader.childNodes[2]);
-}
+// var blogHeader = document.querySelectorAll('.entry-header');
+//
+// for (var i = 0 ; i < blogHeader.length ; i++ ) {
+//     var currentHeader = blogHeader[i];
+//     var headerImg = currentHeader.querySelector('img');
+//     if (!headerImg) {
+//         continue;
+//     }
+//
+//     var blogImgWrap = document.createElement('div');
+//     blogImgWrap.appendChild(headerImg);
+//     currentHeader.insertBefore(blogImgWrap, currentHeader.childNodes[2]);
+// }
