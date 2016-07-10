3ac5cdb900859556ca277d02cf18a45efc41fb4a
Amanda Wong <Amanda Wong>
2016-07-09 21:10:02 -0700

    Wrap blog img in div

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict';
  /*jshint unused:false*/
 
+
+/* Show/Hide Mobile Menu */
 var elMobileMenu = document.getElementById('mobile-menu-container');
 
 window.onresize = function() {
@@ -13,4 +15,20 @@ function showMenu() {
 
 function hideMenu() {
     elMobileMenu.style.left = '-250px';
+}
+
+/* Wrap Div around Post Images */
+
+var blogHeader = document.querySelectorAll('.entry-header');
+
+for (var i = 0 ; i < blogHeader.length ; i++ ) {
+    var currentHeader = blogHeader[i];
+    var headerImg = currentHeader.querySelector('img');
+    if (!headerImg) {
+        continue;
+    }
+
+    var blogImgWrap = document.createElement('div');
+    blogImgWrap.appendChild(headerImg);
+    currentHeader.insertBefore(blogImgWrap, currentHeader.childNodes[2]);
 }
