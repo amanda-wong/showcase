21506a2f6dd77e12a061a1644bfe5516f93479a5
Amanda Wong <Amanda Wong>
2016-07-11 14:06:45 -0700

    Rearrange elements

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,10 +12,11 @@
 		<?php if ( has_post_thumbnail() ) : ?>
 			<div class="post-img-wrap">
 				<?php the_post_thumbnail( 'large' ); ?>
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 			</div>
 		<?php endif; ?>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
 
 		<?php if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
