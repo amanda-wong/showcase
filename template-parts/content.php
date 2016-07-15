cb26294e87cd5990cc46e4ef0664c81842c0817f
Amanda Wong <Amanda Wong>
2016-07-14 22:07:04 -0700

    Center meta info in img post

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,14 +9,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<div class="post-img-wrap">
-				<?php the_post_thumbnail( 'large' ); ?>
-				<div class="entry-meta">
-					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-					<?php red_starter_posted_on(); ?>
-				</div>
-			</div>
+		<?php if ( has_post_thumbnail() ) : echo '<a class="img-link" href="' . get_permalink($post->ID) . '" >';
+  		the_post_thumbnail();
+  		echo '</a>';?>
+
+  		<div class="entry-meta">
+	  		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">',
+			esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	  		<?php red_starter_posted_on(); ?>
+  		</div>
+
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 </article><!-- #post-## -->
