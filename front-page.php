74a3b15d4baba453fb9ba663d1bbc9af8a5e1a8e
Amanda Wong <missamandawong@gmail.com>
2016-12-01 23:57:19 -0800

    Style front page and navigation

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 			<?php get_header(); ?>
-			<div>
+			<div class="canvas">
 				Greetings human, you've come to the right place.
 				Come stay a while and enjoy this open space.
 			</div>
