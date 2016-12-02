4e3ed1d17fabced230873d06ed7e4c9a6a7f440e
Amanda Wong <missamandawong@gmail.com>
2016-12-01 21:08:36 -0800

    Removed old front-page markup

--- front-page.php
+++ front-page.php
@@ -1,16 +1,9 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<div class="page-hero">
-			<?php
+			<?php get_header(); ?>
+			<div>
 
-			get_header(); ?>
-			<div class="welcome-message">
-				<h1>Hi, I'm Amanda and I'm a front-end web developer.<br>
-					Come check out my work!
-				</h1>
-				<a href="/portfolio" class="fp-cta">Portfolio</a>
 			</div>
-		</div>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
