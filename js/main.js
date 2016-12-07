9051efc15f8cc557176b32f93bf63270263fc9a6
Amanda Wong <missamandawong@gmail.com>
2016-12-06 21:09:44 -0800

    Removed unused scripts

--- js/main.js
+++ js/main.js
@@ -23,29 +23,3 @@ function showMenu() {
 function hideMenu() {
     elMobileMenu.style.left = '-250px';
 }
-
-/* Wrap Div Around Post Images */
-
-// var blogHeader = document.querySelectorAll('.entry-header');
-//
-// for (var i = 0 ; i < blogHeader.length ; i++ ) {
-//     var currentHeader = blogHeader[i];
-//     var headerImg = currentHeader.querySelector('img');
-//     if (!headerImg) {
-//         continue;
-//     }
-//
-//     var blogImgWrap = document.createElement('div');
-//     blogImgWrap.appendChild(headerImg);
-//     currentHeader.insertBefore(blogImgWrap, currentHeader.childNodes[2]);
-// }
-
-//Inject HTML content into Text Widget
-var textwidget = document.getElementsByClassName('textwidget')[0];
-var footerSocial = document.querySelectorAll('.social-icon-wrap a');
-
-for (var i = 1; i < footerSocial.length; i++) {
-    var currentIcon = footerSocial[i];
-    var iconCopy = currentIcon.cloneNode(true);
-    textwidget.appendChild(iconCopy);
-}
