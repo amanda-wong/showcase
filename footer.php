a82a84ba13bde9797b51f6ad277c546726bac5fe
Amanda Wong <missamandawong@gmail.com>
2016-12-17 14:07:52 -0800

    Added soundcloud icon to footer

--- footer.php
+++ footer.php
@@ -13,20 +13,23 @@
 				<div class="site-info">
 		            <h2>- Follow me -</h2>
 		            <div class="social-icon-wrap">
-						<a href="https://www.github.com/amanda-wong/" target="_blank">
+						<a href="https://www.github.com/amanda-wong/" target="_blank" title="GitHub">
 		                    <i class="fa fa-github" aria-hidden="true"></i>
 		                </a>
-						<a href="https://www.linkedin.com/in/missamandawong" target="_blank">
+						<a href="https://www.linkedin.com/in/missamandawong" target="_blank" title="LinkedIn">
 		                    <i class="fa fa-linkedin" aria-hidden="true"></i>
 		                </a>
-		                <a href="https://www.facebook.com/amanda.wong.184" target="_blank">
+		                <a href="https://www.facebook.com/amanda.wong.184" target="_blank" title="Facebook">
 		                    <i class="fa fa-facebook" aria-hidden="true"></i>
 		                </a>
-		                <a href="https://twitter.com/missamandawong" target="_blank">
+		                <a href="https://twitter.com/missamandawong" target="_blank" title="Twitter">
 		                    <i class="fa fa-twitter" aria-hidden="true"></i>
 		                </a>
-		                <a href="https://www.instagram.com/missamandawong/" target="_blank">
+		                <a href="https://www.instagram.com/missamandawong/" target="_blank" title="Instagram">
 		                    <i class="fa fa-instagram" aria-hidden="true"></i>
+		                </a>
+						<a href="https://soundcloud.com/amanda-wong-16" target="_blank" title="SoundCloud">
+		                    <i class="fa fa-soundcloud" aria-hidden="true"></i>
 		                </a>
 		            </div>
 		            <div class="copyright">
