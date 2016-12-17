4ed705125e1038e6256ced607c66f9842b2200b7
Amanda Wong <missamandawong@gmail.com>
2016-12-17 13:50:13 -0800

    Add skills bar and functionality

--- about.php
+++ about.php
@@ -7,8 +7,84 @@
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php get_header(); ?>
-        <section class="canvas instagram">
-
+        <section class="canvas">
+            <div class="about-copy">
+                Hi I'm Amanda,
+            </div>
+            <div class="skills-container">
+                <div class="skill">
+                    <div class="skill-label">
+                        HTML
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="80%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        CSS
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="80%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        jQuery
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="20%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        JavaScript
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="25%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        Sass/Less
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="85%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        Gulp
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="60%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        Angular/React
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="5%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        WordPress
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="30%"></div>
+                    </div>
+                </div>
+                <div class="skill">
+                    <div class="skill-label">
+                        Git
+                    </div>
+                    <div class="skill-bar">
+                        <div class="skill-level" data-percent="40%"></div>
+                    </div>
+                </div>
+            </div>
         </section>
 	</main><!-- #main -->
 </div><!-- #primary -->
--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 'use strict';
  /*jshint unused:false*/
 
+/* skills bar css animation percentage display */
+var skillEl = document.querySelectorAll('.skill-level');
+window.onload = function() {
+    skillEl.forEach(function(el){
+        var widthCallBack = setInterval(function(){
+            var parentWidth = el.parentNode.scrollWidth;
+            var width = el.scrollWidth;
+            var percentage = width/parentWidth * 100;
+            percentage = Math.floor(percentage);
+            el.setAttribute('data-percent', percentage + '%');
 
-// Footer copyright year
-var date = new Date();
-var year = date.getFullYear();
-var yearEl = document.getElementById('year');
-yearEl.innerHTML = year;
+            if(percentage == el.style.width.replace('%','')) {
+                clearInterval(widthCallBack);
+            }
+        }, 100);
+        var skillpercent = el.getAttribute('data-percent');
+        el.style.width = skillpercent;
+    });
+};
+
 
 
 /* Show/Hide Mobile Menu */
@@ -23,3 +37,10 @@ function showMenu() {
 function hideMenu() {
     elMobileMenu.style.left = '-250px';
 }
+
+
+// Footer copyright year
+var date = new Date();
+var year = date.getFullYear();
+var yearEl = document.getElementById('year');
+yearEl.innerHTML = year;
