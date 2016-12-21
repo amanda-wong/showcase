5359cb5bbd73d217fcd2a57366149caf8c97d051
Amanda Wong <missamandawong@gmail.com>
2016-12-20 21:43:56 -0800

    Create modal functionality general and styles

--- js/instagram.js
+++ js/instagram.js
@@ -10,19 +10,49 @@ jQuery(document).ready(function($){
 		success: function(data){
 			var instaPosts = data.data;
 			var instagrid = $(document.getElementById('instagram-grid'));
-
 			instaPosts.forEach(function(el){
 				var tags = el.tags;
 				if(tags) {
 					tags.forEach(function(i){
 						if(i == 'amandaoutandabout') {
 							var imgURL = el.images.standard_resolution.url;
-							var img = '<li class="insta-block"><a href="' + imgURL + '" class="insta-url"><img src="' + imgURL + '" class="insta-img"/></a></li>';
+							var img = '<li class="insta-block"><a href="' + imgURL + '" class="insta-link"><img src="' + imgURL + '" class="insta-img"/></a></li>';
 							instagrid.append(img);
+
 						}
 					});
 				}
 			});
+
+			var imageLink = document.querySelectorAll('.insta-link');
+			imageLink.forEach(function(igLink){
+				igLink.addEventListener('click', function(e){
+					e.preventDefault();
+					var instaDetails = document.createElement('div');
+					instaDetails.className = 'modalBackground';
+					var detailsWrap = document.createElement('ul');
+					detailsWrap.className = 'detailsOpen';
+
+
+					// detailsWrap.append($(this).href);
+					instaDetails.appendChild(detailsWrap);
+
+					document.body.appendChild(instaDetails);
+
+				});
+			});
 		}
 	});
 });
+
+// imgURL = $(this).href;
+// console.log(imgURL);
+// instaPosts.forEach(function(el){
+// 	var imgURL = el.images.standard_resolution.url;
+// 	var fullImage = '<li><img src="' + imgURL + '"></></li>';
+// 	var fullDetails = document.createElement('li');
+// 	var caption = el.caption.text;
+// 	fullDetails.innerHTML = caption;
+// 	detailsWrap.innerHTML = fullImage + fullDetails;
+// 	instaDetails.innerHTML = detailsWrap;
+// });
