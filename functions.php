9f180d2135b5d95842f324730632bc3dbd4a1579
Amanda Wong <missamandawong@gmail.com>
2017-01-01 21:11:45 -0800

    Reduce excerpt length

--- functions.php
+++ functions.php
@@ -123,3 +123,14 @@ function new_excerpt_more($more) {
 	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> ...read more.</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
+
+/**
+ * Filter the except length to 20 words.
+ *
+ * @param int $length Excerpt length.
+ * @return int (Maybe) modified excerpt length.
+ */
+function wpdocs_custom_excerpt_length( $length ) {
+    return 20;
+}
+add_filter( 'excerpt_length', 'wpdocs_custom_excerpt_length', 999 );
