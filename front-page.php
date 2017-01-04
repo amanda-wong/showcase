28f06158f0c235b3cac0d006924631d7cbf19a6e
Amanda Wong <missamandawong@gmail.com>
2017-01-03 23:41:36 -0800

    Add and style homepage hero

--- front-page.php
+++ front-page.php
@@ -1,10 +1,15 @@
+
+
 <div id="primary" class="content-area">
+	<?php get_header(); ?>
 	<main id="main" class="site-main" role="main">
-			<?php get_header(); ?>
-			<section class="canvas">
+		<div class="hp-hero-wrap"></div>
+		<section class="canvas">
+			<div class="hp-spottext">
 				<p>Hello</p>
 				<p>I'm Amanda and I'm a front-end developer</p>
-			</section>
+			</div>
+		</section>
 	</main><!-- #main -->
 </div><!-- #primary -->
 
