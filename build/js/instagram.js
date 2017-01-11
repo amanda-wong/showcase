ad997c6bf3964c1560c590e2f7ae35e106caa846
Amanda Wong <missamandawong@gmail.com>
2017-01-10 21:13:23 -0800

    Clean up spacing

--- build/js/instagram.js
+++ build/js/instagram.js
@@ -41,6 +41,8 @@ function handleApiResponse(response) {
 		}
 	});
 
+	console.log(instaPosts);
+
 	//Limit post count to 12
 	instaPosts = instaPosts.slice(0,12);
 	instaPosts.forEach(function(el){
--- build/js/main.js
+++ build/js/main.js
@@ -4,47 +4,36 @@
 /* skills bar css animation percentage display */
 var skillEl = document.querySelectorAll('.skill-level');
 window.onload = function() {
-    skillEl.forEach(function(el){
-        var widthCallBack = setInterval(function(){
-            var parentWidth = el.parentNode.scrollWidth;
-            var width = el.scrollWidth;
-            var percentage = width/parentWidth * 100;
-            percentage = Math.floor(percentage);
-            el.setAttribute('data-percent', percentage + '%');
+	skillEl.forEach(function(el){
+		var widthCallBack = setInterval(function(){
+			var parentWidth = el.parentNode.scrollWidth;
+			var width = el.scrollWidth;
+			var percentage = width/parentWidth * 100;
+			percentage = Math.floor(percentage);
+			el.setAttribute('data-percent', percentage + '%');
 
-            if(percentage == el.style.width.replace('%','')) {
-                clearInterval(widthCallBack);
-            }
-        }, 100);
-        var skillpercent = el.getAttribute('data-percent');
-        el.style.width = skillpercent;
-    });
+			if(percentage == el.style.width.replace('%','')) {
+				clearInterval(widthCallBack);
+			}
+		}, 100);
+		var skillpercent = el.getAttribute('data-percent');
+		el.style.width = skillpercent;
+	});
 };
-
-
 
 /* Show/Hide Mobile Menu */
 var elMobileMenu = document.getElementById('mobile-menu-container');
 
 window.onresize = function() {
-    hideMenu();
-};
-
-window.onload = function() {
 	hideMenu();
 };
 
 function showMenu() {
-	document.body.style.overflowY = 'hidden';
 	elMobileMenu.style.left = '0px';
-
-
 }
 
 function hideMenu() {
-	document.body.style.overflowY = 'auto';
-    elMobileMenu.style.left = '-500px';
-
+	elMobileMenu.style.left = '-250px';
 }
 
 
