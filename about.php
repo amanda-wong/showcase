2b8c01707f29b673334cecd206a3cf824d60d518
Amanda Wong <missamandawong@gmail.com>
2017-03-15 20:33:34 -0700

    Make edits to about info section

--- about.php
+++ about.php
@@ -26,8 +26,8 @@
 							<li>I listen to nothing but electronic music.</li>
 							<li>I twirl my hair A LOT and unintentionally use it as a mustache.</li>
 							<li>I never considered myself a gamer until I played Skyrim.</li>
-							<li>I strive to be a full-stack web developer one day.</li>
-							<li>I truly feel guilty if I don't compost or recycle.</li>
+							<li>I aim to be a full-stack web developer one day.</li>
+							<li>I feel so guilty if I don't compost or recycle.</li>
 						</ul>
 					</div>
 				</div>
